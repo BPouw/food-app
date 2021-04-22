2828eb9febc000daee0ee42a16439976b832491c
Boris Pouw <borispouw@hotmail.com>
2021-04-22 12:26:34 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = proces.env || 3000
+
+app.get('/', (req, res) => {
+  let result = {
+    name : "Jimmy"
+  }
+  res.status(418)
+  res.json(result)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
