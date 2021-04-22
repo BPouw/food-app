d540764fe7d4cca490681d185371b9cd20a6b64e
Boris Pouw <borispouw@hotmail.com>
2021-04-22 13:16:28 +0200

    info tab

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.get('/', (req, res) => {
   res.json(result)
 })
 
+app.get('/info', (req, res) => {
+  let result = {
+    naam : "Boris Pouw",
+    studentnr : 2116083,
+    beschrijving: "Dit is de backend van een app om eten te delen",
+    url: "N/A"
+  }
+  res.status(200)
+  res.json(result)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
