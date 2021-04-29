db0e75674045cc6d35e2aecc95c60950a21fac59
Boris Pouw <borispouw@hotmail.com>
2021-04-29 14:37:37 +0200

    server.js update

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const mealroutes = require("./src/routes/meal-routes")
+const homeroutes = require("./src/routes/studenthome-routes")
+
+app.use(express.json())
+
 app.all("*", (req, res, next) => {
   const reqMethod = req.method
   const reqUrl = req.originalUrl
@@ -9,13 +14,7 @@ app.all("*", (req, res, next) => {
   next()
 })
 
-app.get('/', (req, res) => {
-  let result = {
-    name : "Jimmy"
-  }
-  res.status(418)
-  res.json(result)
-})
+app.use("/api", mealroutes, homeroutes)
 
 app.get('/info', (req, res) => {
   let result = {
@@ -45,4 +44,4 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-module.exports = server;
+module.exports = app;
